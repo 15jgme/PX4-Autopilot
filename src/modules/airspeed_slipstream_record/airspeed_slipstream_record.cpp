6d148d59f762a5613d944858dabfe9433eb2b233
Jackson <jackson.empey@mail.mcgill.ca>
2021-07-08 18:04:54 -0400

    first crack

---
+++ src/modules/airspeed_slipstream_record/airspeed_slipstream_record.cpp
@@ -0,0 +1,287 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2018 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#include "airspeed_slipstream_record.h"
+
+#include <px4_platform_common/getopt.h>
+#include <px4_platform_common/log.h>
+#include <px4_platform_common/posix.h>
+
+#include <uORB/topics/parameter_update.h>
+#include <uORB/topics/sensor_combined.h>
+#include <uORB/uORB.h>
+
+
+int TemplateModule::print_status()
+{
+	PX4_INFO("Running");
+	// TODO: print additional runtime information about the state of the module
+
+	return 0;
+}
+
+int TemplateModule::custom_command(int argc, char *argv[])
+{
+	/*
+	if (!is_running()) {
+		print_usage("not running");
+		return 1;
+	}
+	// additional custom commands can be handled like this:
+	if (!strcmp(argv[0], "do-something")) {
+		get_instance()->do_something();
+		return 0;
+	}
+	 */
+
+	return print_usage("unknown command");
+}
+
+
+int TemplateModule::task_spawn(int argc, char *argv[])
+{
+	_task_id = px4_task_spawn_cmd("module",
+				      SCHED_DEFAULT,
+				      SCHED_PRIORITY_DEFAULT,
+				      1024,
+				      (px4_main_t)&run_trampoline,
+				      (char *const *)argv);
+
+	if (_task_id < 0) {
+		_task_id = -1;
+		return -errno;
+	}
+
+	return 0;
+}
+
+TemplateModule *TemplateModule::instantiate(int argc, char *argv[])
+{
+	int example_param = 0;
+	bool example_flag = false;
+	bool error_flag = false;
+
+	int myoptind = 1;
+	int ch;
+	const char *myoptarg = nullptr;
+
+	// parse CLI arguments
+	while ((ch = px4_getopt(argc, argv, "p:f", &myoptind, &myoptarg)) != EOF) {
+		switch (ch) {
+		case 'p':
+			example_param = (int)strtol(myoptarg, nullptr, 10);
+			break;
+
+		case 'f':
+			example_flag = true;
+			break;
+
+		case '?':
+			error_flag = true;
+			break;
+
+		default:
+			PX4_WARN("unrecognized flag");
+			error_flag = true;
+			break;
+		}
+	}
+
+	if (error_flag) {
+		return nullptr;
+	}
+
+	TemplateModule *instance = new TemplateModule(example_param, example_flag);
+
+	if (instance == nullptr) {
+		PX4_ERR("alloc failed");
+	}
+
+	return instance;
+}
+
+TemplateModule::TemplateModule(int example_param, bool example_flag)
+	: ModuleParams(nullptr)
+{
+}
+
+void TemplateModule::run()
+{
+	/* advertise attitude topic */
+	struct airspeed_multi_record_s airspeed_multi_data;
+	memset(&airspeed_multi_data, 0, sizeof(airspeed_multi_data));
+	orb_advert_t att_pub = orb_advertise(ORB_ID(airspeed_multi_record), &airspeed_multi_data);
+
+	// Example: run the loop synchronized to the sensor_combined topic publication
+	int sensor_combined_sub = orb_subscribe(ORB_ID(sensor_combined)); // OOP way --> https://github.com/PX4/PX4-Autopilot/blob/master/src/modules/mavlink/mavlink_receiver.h
+
+	/* subscribe to vehicle_acceleration topic */
+	int sensor_sub_fd = orb_subscribe(ORB_ID(differential_pressure));
+	/* subscribe to esc rpm topic */
+	int esc_sub_fd = orb_subscribe(ORB_ID(esc_status));
+	/* subscribe to airspeed topic */
+	int asp_sub_fd = orb_subscribe(ORB_ID(airspeed));
+	/* subscribe to rc topic */
+	int rc_sub_fd = orb_subscribe(ORB_ID(rc_channels));
+
+		px4_pollfd_struct_t fds[] = {
+		{ .fd = sensor_combined_sub,   .events = POLLIN },
+		{ .fd = sensor_sub_fd,   .events = POLLIN },
+		{ .fd = esc_sub_fd,   .events = POLLIN },
+		{ .fd = asp_sub_fd,   .events = POLLIN },
+		{ .fd = rc_sub_fd,   .events = POLLIN },
+	};
+
+
+	// initialize parameters
+	parameters_update(true);
+
+	while (!should_exit()) {
+
+		// wait for up to 1000ms for data
+		int pret = px4_poll(fds, (sizeof(fds) / sizeof(fds[0])), 1000);
+
+		if (pret == 0) {
+			// Timeout: let the loop run anyway, don't do `continue` here
+
+		} else if (pret < 0) {
+			// this is undesirable but not much we can do
+			PX4_ERR("poll error %d, %d", pret, errno);
+			px4_usleep(50000);
+			continue;
+
+		} else if (fds[0].revents & POLLIN) {
+
+			struct sensor_combined_s sensor_combined;
+			orb_copy(ORB_ID(sensor_combined), sensor_combined_sub, &sensor_combined);
+			// TODO: do something with the data...
+
+			struct differential_pressure_s diff_pres_ID_1; //Struct to store data for primary sensor
+			struct differential_pressure_s diff_pres_ID_2; //Struct to store data for slipstream sensor
+			struct differential_pressure_s diff_pres;
+			struct esc_status_s esc_stat;
+			struct airspeed_s airspeed;
+			struct rc_channels_s rc_chan;
+
+
+
+			orb_copy(ORB_ID(rc_channels), rc_sub_fd, &rc_chan);
+			if((int)rc_chan.channels[7] == 1)	//RECORD!
+			{
+				orb_copy(ORB_ID(differential_pressure), sensor_sub_fd, &diff_pres);
+				if(diff_pres.device_id == sensID_1 && sens_1_active){
+					diff_pres_ID_1 = diff_pres;
+				} else if (diff_pres.device_id == sensID_2 && sens_2_active) {
+					diff_pres_ID_2 = diff_pres;
+					PX4_INFO("secondary");
+				} else if (sens_1_active || sens_2_active) {
+					//Not good, correct device id's arent set
+					PX4_ERR("Incorrect airspeed sensor ID detected, please check");
+					errFlag = true;
+				}
+
+
+				orb_copy(ORB_ID(esc_status), esc_sub_fd, &esc_stat);
+				orb_copy(ORB_ID(airspeed), asp_sub_fd, &airspeed);
+
+				//Put system IAS in message
+				airspeed_multi_data.vehicle_ias = airspeed.indicated_airspeed_m_s; //Set airspeed from system to msg airspeed (for faster logging)
+
+				//CALCULATE AIRSPEED HERE AND PUBLISH
+				airspeed_ID_1 = diff_pres.differential_pressure_filtered_pa;
+				airspeed_ID_2 = 4.0f;
+
+				airspeed_multi_data.primary_airspeed_ms = airspeed_ID_1;
+				airspeed_multi_data.secondary_airspeed_ms = airspeed_ID_2;
+
+				airspeed_multi_data.rpm_sens = esc_stat.esc[0].esc_rpm;
+				// airspeed_multi_data.rpm_sens = 5;
+
+				airspeed_multi_data.timestamp = hrt_absolute_time();
+
+				orb_publish(ORB_ID(airspeed_multi_record), att_pub, &airspeed_multi_data);
+			}
+
+
+		}
+
+		parameters_update();
+	}
+
+	orb_unsubscribe(sensor_combined_sub);
+}
+
+void TemplateModule::parameters_update(bool force)
+{
+	// check for parameter updates
+	if (_parameter_update_sub.updated() || force) {
+		// clear update
+		parameter_update_s update;
+		_parameter_update_sub.copy(&update);
+
+		// update parameters from storage
+		updateParams();
+	}
+}
+
+int TemplateModule::print_usage(const char *reason)
+{
+	if (reason) {
+		PX4_WARN("%s\n", reason);
+	}
+
+	PRINT_MODULE_DESCRIPTION(
+		R"DESCR_STR(
+### Description
+Section that describes the provided module functionality.
+This is a template for a module running as a task in the background with start/stop/status functionality.
+### Implementation
+Section describing the high-level implementation of this module.
+### Examples
+CLI usage example:
+$ module start -f -p 42
+)DESCR_STR");
+
+	PRINT_MODULE_USAGE_NAME("module", "template");
+	PRINT_MODULE_USAGE_COMMAND("start");
+	PRINT_MODULE_USAGE_PARAM_FLAG('f', "Optional example flag", true);
+	PRINT_MODULE_USAGE_PARAM_INT('p', 0, 0, 1000, "Optional example parameter", true);
+	PRINT_MODULE_USAGE_DEFAULT_COMMANDS();
+
+	return 0;
+}
+
+int template_module_main(int argc, char *argv[])
+{
+	return TemplateModule::main(argc, argv);
+}
